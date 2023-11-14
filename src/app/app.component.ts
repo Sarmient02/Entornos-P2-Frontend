3168121a043efc942614196e7a5d219f69ae17a9
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-11-13 21:54:51 -0500

    full standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [InputTextModule, CheckboxModule, ButtonModule]
 })
 export class AppComponent {
   title = 'entornos-p2-frontend';
