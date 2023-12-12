865f2fabdd504e9b6d4b7ba4002a338603ee53b2
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-12-11 22:08:05 -0500

    follow users and comment posts

--- src/app/modules/post/pages/view/view.component.ts
+++ src/app/modules/post/pages/view/view.component.ts
@@ -14,17 +14,58 @@ import { PostService } from 'src/app/services/post.service';
 import { FileP } from 'src/app/models/file.model';
 import { FileService } from 'src/app/services/file.service';
 import { SkeletonModule } from 'primeng/skeleton';
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FollowService } from 'src/app/services/follow.service';
+import { CommentService } from 'src/app/services/comment.service';
+import { Comment, newComment } from 'src/app/models/comment.model';
+import { CardModule } from 'primeng/card';
+import { RatingModule, RatingRateEvent } from 'primeng/rating';
+import { DividerModule } from 'primeng/divider';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputText, InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [CommonModule, RouterModule, TagModule, AvatarModule, FileUploadModule, MimetypePipe, SplitButtonModule, SkeletonModule],
+  imports: [CommonModule, RouterModule, TagModule, AvatarModule, FileUploadModule, MimetypePipe, SplitButtonModule,
+            SkeletonModule, BadgeModule, ButtonModule, CardModule, RatingModule, DividerModule, InputTextareaModule, ReactiveFormsModule, FormsModule
+  ],
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.scss'],
-  providers: [DialogService]
+  providers: [DialogService, BrowserAnimationsModule]
 })
 export class ViewComponent {
+
+  private fb = inject(FormBuilder);
+
+  private comment: newComment = {} as newComment;
+
+  followed = {
+    "backgroundColor": "#007bff",
+    "color": "#fff"
+  }
+
+  following = false;
+
+  unfollowed = {
+    "backgroundColor": "#fff",
+    "color": "#007bff"
+  }
+
+  form = this.fb.group({
+    id: ['0'],
+    body: ['', [Validators.required, Validators.minLength(3)]],
+    postId: [new FormControl()],
+    idUser: [new FormControl()],
+    score: ['']
+  });
+
+  comments!: Comment[];
 
   ref: DynamicDialogRef | undefined;
 
@@ -41,12 +82,15 @@ export class ViewComponent {
     public dialogService: DialogService,
     private postService: PostService,
     private fileService: FileService,
-    private router: Router
+    private router: Router,
+    private followService: FollowService,
+    private commentService: CommentService
   ) {
   }
 
   ngOnInit() {
     this.getPost(this.idPost);
+    this.checkFollow();
   }
 
   getPost(id: string) {
@@ -55,7 +99,8 @@ export class ViewComponent {
         next: (data) => {
           this.post = data;
           console.log(data)
-          this.loading = false;
+          this.checkFollow();
+          
         },
         error: (e) => {
           console.log(e)
@@ -109,6 +154,137 @@ export class ViewComponent {
     if (!sessionStorage.getItem('user')) return false;
     return this.post.user.id === JSON.parse(sessionStorage.getItem('user')!).id;
   }
+
+  ifUserIsCommentAuthor(id: number) {
+    if (!sessionStorage.getItem('user')) return false;
+    return id === JSON.parse(sessionStorage.getItem('user')!).id;
+  }
+
+  followOrUnfollow() {
+    if(this.following) this.unfollow();
+    else this.follow();
+  }
+
+  follow() {
+      if(!this.ifUserIsAuthor()){
+        this.followService.follow(JSON.parse(sessionStorage.getItem('user')!).id.toString(), this.post.user.id.toString())
+        .subscribe({
+          next: () => {
+            this.following = true;
+          },
+          error: (e) => {
+            console.log(e)
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: e.error.message });
+          }
+        });
+      }
+  }
+
+  unfollow() {
+    if(!this.ifUserIsAuthor()){
+      this.followService.unfollow(JSON.parse(sessionStorage.getItem('user')!).id.toString(), this.post.user.id.toString())
+      .subscribe({
+        next: () => {
+          this.following = false;
+        },
+        error: (e) => {
+          console.log(e)
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: e.error.message });
+        }
+      });
+    }
+  }
+
+  getFollowStyle() {
+    if(this.following) return this.followed;
+    return this.unfollowed;
+  }
+
+  checkFollow() {
+    if(!this.ifUserIsAuthor()){
+      this.followService.isFollowing(JSON.parse(sessionStorage.getItem('user')!).id.toString(), this.post.user.id.toString())
+      .subscribe({
+        next: (data) => {
+          this.following = data;
+        },
+        error: (e) => {
+          console.log(e)
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: e.error.message });
+        }
+      });
+    }
+    this.getComments();
+  }
+
+  getComments() {
+    this.commentService.getAllComments(this.post.id.toString())
+    .subscribe({
+      next: (data) => {
+        this.comments = data;
+        this.loading = false;
+        console.log(this.comments)
+      },
+      error: (e) => {
+        console.log(e)
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: e.error.message });
+      }
+    });
+  }
+
+  onSubmit(): void {
+    console.log("aaaaaaa")
+    console.log(this.form)
+    this.setForm();
+    console.log(this.form)
+    if(this.form.valid) {
+      const { body, postId, idUser, score } = this.form.getRawValue();
+      if (!body || !postId || !idUser) {
+        return;
+      }
+      this.comment = Object.assign(this.comment, this.form.value);
+      console.log("new comment")
+      console.log(this.comment)
+      this.commentService.createComment(this.comment)
+      .subscribe({
+        next: (data) => {
+          this.messageService.add({ key:'br', severity: 'info', summary: 'Correcto', detail: 'Comentario creado exitosamente' });
+          this.getComments();
+          this.form.reset();
+          this.setForm();
+        },
+        error: () => {
+          this.messageService.add({severity:'error', summary: 'Error', detail: 'Error al crear el comentario'});
+        }
+      });
+    } else {
+      this.form.markAllAsTouched();
+    }
+    }
   
 
+    setRate(evento: RatingRateEvent){
+      this.form.value.score = evento.value.toString();
+      console.log(this.form.value.score)
+    }
+
+    setForm(){
+      this.form.value.postId = this.post.id;
+      this.form.value.idUser = JSON.parse(sessionStorage.getItem('user')!).id;
+      this.form.value.id = '0';
+      this.comment = {} as newComment;
+    }
+
+    deleteComment(id: string) {
+      this.commentService.deleteComment(id)
+      .subscribe({
+        next: (data) => {
+          this.messageService.add({ key:'br', severity: 'info', summary: 'Correcto', detail: 'Comentario eliminado exitosamente' });
+          this.getComments();
+        },
+        error: () => {
+          this.messageService.add({severity:'error', summary: 'Error', detail: 'Error al eliminar el comentario'});
+        }
+      });
+    }
+      
 }
--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Comment, newComment } from '../models/comment.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
+  private options = {
+    withCredentials: true // Asegúrate de incluir las credenciales en la solicitud
+  };
+
+  private API_URL = 'http://localhost:8080/api';
+
+  private http = inject(HttpClient);
+
   constructor() { }
+
+  getAllComments(idPost: string){
+    const url = `${this.API_URL}/post/comments?postId=${idPost}`;
+    console.log(url)
+    return this.http.get<Comment[]>(url, this.options)
+  }
+
+  createComment(comment: newComment){
+    const url = `${this.API_URL}/post/comments-new`;
+    console.log(url)
+    return this.http.post<any>(url, comment, this.options)
+  }
+
+  deleteComment(id: string){
+    const url = `${this.API_URL}/post/comments-delete?commentId=${id}`;
+    console.log(url)
+    return this.http.delete<any>(url, this.options)
+  }
+
 }
